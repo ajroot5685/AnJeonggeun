05c9c6b79d319983068aa8027ed8f2ce7457f170
ajroot5685 <anjeong8577@naver.com>
2023-07-04 23:51:49 +0900

    JavaScript - display items

--- assignment3/Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ assignment3/Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -5,11 +5,25 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+  return `
+  <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+      </li>
+  `;
+}
 // main
 loadItems()
   .then((items) => {
-    console.log(items);
     displayItems(items);
-    setEventListeners(items);
+    // setEventListeners(items);
   })
   .catch(console.log);
