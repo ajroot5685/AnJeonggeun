e283150f9562c7cd0eeb88baa9b2e169c28df90b
ajroot5685 <anjeong8577@naver.com>
2023-07-07 00:48:33 +0900

    파이썬 개인과제:1-3

--- assignment5/파이썬 개인과제/배스킨라빈스31 게임/game.py
+++ assignment5/파이썬 개인과제/배스킨라빈스31 게임/game.py
@@ -1,3 +1,28 @@
+class NumError(Exception):
+    def __init__(self):
+        super().__init__()
+    def check(num):
+        if num==1 or num==2 or num==3:
+            return True
+        else:
+            return False
+        
+
+
 num = 0
 
-count=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+while 1:
+    count=input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+    try:
+        count=int(count)
+    except ValueError:
+        print('정수를 입력하세요')
+        continue
+
+    try:
+        if not NumError.check(count):
+            raise NumError
+    except:
+        print('1, 2, 3 중 하나를 입력하세요')
+        continue
+    break
