6ff195b0626b932bb75aca563ea0a6e34784662d
ajroot5685 <anjeong8577@naver.com>
2023-07-07 00:57:11 +0900

    파이썬 개인과제:1-5

--- assignment5/파이썬 개인과제/배스킨라빈스31 게임/game.py
+++ assignment5/파이썬 개인과제/배스킨라빈스31 게임/game.py
@@ -6,26 +6,41 @@ class NumError(Exception):
             return True
         else:
             return False
-        
+
+global num      
 num = 0
 
-while 1:
-    count=input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
-    try:
-        count=int(count)
-    except ValueError:
-        print('정수를 입력하세요')
-        continue
+def input_num():
+    while 1:
+        global count
+        count=input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+        try:
+            count=int(count)
+        except ValueError:
+            print('정수를 입력하세요')
+            continue
 
-    try:
-        if not NumError.check(count):
-            raise NumError
-    except:
-        print('1, 2, 3 중 하나를 입력하세요')
-        continue
-    break
+        try:
+            if not NumError.check(count):
+                raise NumError
+        except:
+            print('1, 2, 3 중 하나를 입력하세요')
+            continue
+        break
 
+def main():
+    global count
+    global num
 
-for i in range(count):
-    num+=1
-    print(num)
+    input_num()
+
+    for i in range(count):
+        num+=1
+        print("playerA :", num)
+
+    input_num()
+    for i in range(count):
+        num+=1
+        print("playerB :", num)
+
+main()
