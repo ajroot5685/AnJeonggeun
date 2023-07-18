43faed9bac01a13d4b8195d0e4b4c515bfd28f1b
ajroot5685 <anjeong8577@naver.com>
2023-07-19 01:19:23 +0900

    idea_list html 완성

--- assignment10(SWIDEA_SITE)/server/idea/urls.py
+++ assignment10(SWIDEA_SITE)/server/idea/urls.py
@@ -4,9 +4,12 @@ from idea import views
 app_name="idea"
 
 urlpatterns = [    
-    path('', views.idea_list),
-    # path('posts/detail/<int:pk>', views.detail, name='detail'),
-    # path('posts/create', views.create, name='create'),
-    # path('posts/update/<int:pk>', views.update, name='update'),
-    # path('posts/delete/<int:pk>', views.delete, name='delete'),
+    # path('', views.idea_list, name='list'),
+    path('', views.idea_devdetail, name='list'),
+    path('ideas/create', views.idea_create, name='create'),
+    # path('ideas/detail/<int:pk>', views.idea_detail, name='detail'),
+    # path('ideas/devtool/list', views.idea_devlist, name='devlist'),
+    # path('ideas/devtool/register', views.idea_devregister, name='devregister'),
+    # path('ideas/devtool/detail/<int:pk>', views.idea_devdetail, name='idea_devdetail'),
+    
 ]
--- assignment10(SWIDEA_SITE)/server/idea/views.py
+++ assignment10(SWIDEA_SITE)/server/idea/views.py
@@ -8,3 +8,23 @@ def idea_list(request):
     #     'ideas': ideas,
     # }
     return render(request, 'idea/idea_list.html')
+
+def idea_create(request):
+
+    return render(request, 'idea/idea_create.html')
+
+def idea_detail(request):
+
+    return render(request, 'idea/idea_detail.html')
+
+def idea_devlist(request):
+
+    return render(request, 'idea/idea_devlist.html')
+
+def idea_devregister(request):
+
+    return render(request, 'idea/idea_devregister.html')
+
+def idea_devdetail(request):
+
+    return render(request, 'idea/idea_devdetail.html')
